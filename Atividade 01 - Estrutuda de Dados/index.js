608fc017a0a01b1b0c14ab7b7726ca073a65b609
Victorfrts <victor.pe.freitas@gmail.com>
2022-01-24 15:51:32 -0400

    Printar ano + numero

--- Atividade 01 - Estrutuda de Dados/index.js
+++ Atividade 01 - Estrutuda de Dados/index.js
@@ -4,7 +4,7 @@ const numero = 13; // Crie uma variável que receba um número;
 
 const ano = 2022 - numero; // Crie uma variável com o ano atual e subtraia pelo número que você guardou;
 
-console.log(ano) // Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(ano + numero) // Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
 
 console.log(`Estou mexendo no console.log`) // Mostre no console a mensagem "Estou mexendo no console.log";
 
