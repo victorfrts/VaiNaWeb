3dcb9c41cbcebc4f41cc6456f7ad07300bd57ed8
Victorfrts <victor.pe.freitas@gmail.com>
2022-01-26 07:28:26 -0400

    Atividade 05

---
+++ Atividade 05 - Estrutura de dados simples/index.js
@@ -0,0 +1,18 @@
+const itens = ['feijão','salada','batata','arroz','bife']// Crie um array que receba 5 itens e exiba no console.
+console.log(itens)
+
+itens.unshift('Victor') // Utilize um método para adicionar um nome ao inicio do array.
+console.log(itens)
+
+itens.pop()// Utilize um método para remover o último nome do array.
+console.log(itens)
+
+itens.push('Maria', 'Aline') // Utilize um método para adicionar dois nomes ao fim do array.
+console.log(itens)
+
+itens.shift()// Utilize um método para remover o primeiro nome do array.
+console.log(itens)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+const numbers = [7,5,6,3,8,9,2,1,4]
+console.log(numbers.sort())
